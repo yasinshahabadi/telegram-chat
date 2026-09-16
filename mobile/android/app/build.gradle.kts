c1713ca44ed351070339998472cb248809b796c4
yasinshahabadi <yasinshahabadi007@gmail.com>
2026-09-16 13:48:23 +0330

    fix(android): configure Kotlin Gradle plugin and compile debug APK
    Apply org.jetbrains.kotlin.android plugin in build.gradle.kts, configure jvmTarget.set(JVM_17), and successfully assemble debug APK for Guysgram.

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -1,5 +1,6 @@
 ﻿plugins {
     id("com.android.application")
+    id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -35,7 +36,7 @@ dependencies {
 
 kotlin {
     compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 }
 
